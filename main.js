ef8b560569d4e39306a6e6980707b9637d844d0e
telecter <102524245+telecter@users.noreply.github.com>
2025-07-11 07:54:31 +0200

    improve explantation

--- main.js
+++ main.js
@@ -1,32 +1,38 @@
 const counter = document.getElementById("binary-counter");
-const base10Counter = document.getElementById("decimal-counter");
+const decimalCounter = document.getElementById("decimal-counter");
 const bits = {
-    1: false,
-    2: false,
-    4: false,
-    8: false,
-    16: false,
-    32: false,
-    64: false,
-    128: false
+  1: false,
+  2: false,
+  4: false,
+  8: false,
+  16: false,
+  32: false,
+  64: false,
+  128: false,
+};
+
+const elements = document.querySelectorAll("[data-bit]");
+for (const element of elements) {
+  element.onclick = () => {
+    const bit = element.getAttribute("data-bit");
+    flipBit(parseInt(bit));
+  };
 }
 
 function flipBit(bit) {
-    const bitElement = document.getElementById(`bit-${bit}`);
-    bits[bit] = !bits[bit];
+  const element = document.querySelector(`[data-bit="${bit}"]`);
+  bits[bit] = !bits[bit];
 
-    if (bitElement.style.color == "green") {
-        bitElement.style.color = "red";
-    }
-    else if (bitElement.style.color == "red") {
-        bitElement.style.color = "green";
-    }
+  if (element.style.color == "green") {
+    element.style.color = "red";
+  } else if (element.style.color == "red") {
+    element.style.color = "green";
+  }
 
-    let num = 0;
-    for (const [k, v] of Object.entries(bits)) {
-        if (v) num += Number.parseInt(k);
-    }
-    counter.innerText = num.toString(2).padStart(8, '0');
-    base10Counter.innerText = num.toString()
-
+  let num = 0;
+  for (const [k, v] of Object.entries(bits)) {
+    if (v) num += Number.parseInt(k);
+  }
+  counter.innerText = num.toString(2).padStart(8, "0");
+  decimalCounter.innerText = num.toString();
 }
